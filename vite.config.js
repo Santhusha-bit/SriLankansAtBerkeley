c9fd8145ab1cf9592acc4af3b5bff7ba84a8b52d
santhusha <santhusha_janana@berkeley.edu>
2026-04-22 00:50:27 -0700

    Vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // For GitHub Pages: base must match repo name for project sites
 export default defineConfig({
   plugins: [react()],
-  base: '/SriLankansAtBerkeley/',
+  base: '/',        //For github pages '/SriLankansAtBerkeley/'
 })
